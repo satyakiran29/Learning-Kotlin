1029e9a6d8c4c7ae1f2b5809d6d573baaa285747
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2024-03-24 18:22:17 +0530

    if else

---
+++ if else.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val condition = true // You can change this to false to see the else block
+    if (condition) {
+        println("If block is executed")
+    } else {
+        println("Else block is executed")
+    }
+}
